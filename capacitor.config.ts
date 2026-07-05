04cc04930abd1a04b8ae6a25e4b67df4b30a240f
nilerivk <nil.igvi@gmail.com>
2026-07-05 03:37:30 +0200

    Configure Codemagic

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.tuusuario.moneycontrol',
+  appId: "com.nilerivk.moneycontrol",
   appName: 'MoneyControl',
   webDir: 'dist'
 };
